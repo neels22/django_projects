1dd5cf8476bd0b701b789e02382a025178d45920
indraneel sarode <indraneel.sarode@coditas.com>
2024-04-01 11:19:54 +0530

    admin operations

--- apis/myapp/urls.py
+++ apis/myapp/urls.py
@@ -5,7 +5,7 @@ from django.urls import path,include
 from .views import StudentView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
     path('student/', StudentView.as_view()),
     path('student/<int:rollno>', StudentView.as_view()),
 ]
